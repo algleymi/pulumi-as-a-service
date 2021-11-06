5cd9dc95fbf9fcc14dc2492afb317e29bc860920
Arno Schutijzer <arno.schutijzer@skryv.com>
2021-11-07 00:41:29 +0100

    test(pulumi-configuration): add test for LocalWorkspace options

--- pulumi-configuration.test.ts
+++ pulumi-configuration.test.ts
@@ -1,5 +1,7 @@
-import { Environment } from "@pulumi/aws/appconfig";
-import { validateEnvironmentVariables } from "./pulumi-configuration";
+import {
+  validateEnvironmentVariables,
+  workspaceOptionsWith,
+} from "./pulumi-configuration";
 
 describe("pulumi-configuration", () => {
   it("throws an error when the pulumi configuration is invalid", () => {
@@ -13,10 +15,31 @@ describe("pulumi-configuration", () => {
       PULUMI_BACKEND_URL: "some-backend-url",
       KMS_KEY_ALIAS: "some-kms-key-alias",
     };
-
     process.env = NEW_ENV;
 
     expect(() => validateEnvironmentVariables()).not.toThrow();
+
+    process.env = OLD_ENV;
+  });
+
+  it("returns the stack configuration for a given stack", () => {
+    const OLD_ENV = process.env;
+    const NEW_ENV = {
+      ...OLD_ENV,
+      KMS_KEY_ALIAS: "some-kms-key-alias",
+    };
+    process.env = NEW_ENV;
+
+    expect(workspaceOptionsWith("arnos-stack")).toMatchInlineSnapshot(`
+      Object {
+        "secretsProvider": "some-kms-key-alias",
+        "stackSettings": Object {
+          "arnos-stack": Object {
+            "secretsProvider": "some-kms-key-alias",
+          },
+        },
+      }
+    `);
 
     process.env = OLD_ENV;
   });
