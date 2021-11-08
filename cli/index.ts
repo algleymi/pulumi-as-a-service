32aaf31f05f34c71db515180a83e8b5cd7b3e7ed
Arno Schutijzer <arno.schutijzer@skryv.com>
2021-11-08 22:36:08 +0100

    add placeholder cli implementation

--- cli/index.ts
+++ cli/index.ts
@@ -1,1 +1,15 @@
-console.info("hello world");
+#!/usr/bin/env node
+
+import yargs from "yargs";
+import * as useCases from "@night_city.ts/core/use-cases";
+
+yargs
+  .scriptName("night_city")
+  .usage("$0 <cmd> [args]")
+  .command("hello", "Logs hello world.", function () {
+    console.info("hello world");
+  })
+  .command("storage", "Creates the storage stack", async function () {
+    await useCases.deleteStorage("arno-deleteme");
+  })
+  .help().argv;
